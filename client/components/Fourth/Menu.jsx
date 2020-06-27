4e8ef2f77491d34b1902cb5b6aaafee2eb8d9593
Mark Fuechec <fuechec.mark@gmail.com>
2020-06-27 15:25:26 -0500

    Updated menu in landscape view

--- client/components/Fourth/Menu.jsx
+++ client/components/Fourth/Menu.jsx
@@ -4,6 +4,19 @@ export default function Menu() {
   return (
     <div id="menu">
       <div className="text title">Menu</div>
+      <div className='text subTitle'>Food</div>
+      <div className='text left item'>Chicken Wings</div>
+      <div className='text left wings'>* Teryaki</div>
+      <div className='text left wings'>* Lemon Pepper</div>
+      <div className='text left wings'>* Paprika Rubbed</div>
+      <div className='text left item'>Chips and Guac</div>
+      <div className='text left item'>Watermelon</div>
+      <div className='text left item'>Corn on the Cob</div>
+      <div className='text left item'>Caesar Salad</div>
+      <div className='text subTitle'>Drinks</div>
+      <div className='text left item'>Tom Collins</div>
+      <div className='text left item'>Beer</div>
+      <div className='text left item'>Lemonade</div>
     </div>
   );
 }
